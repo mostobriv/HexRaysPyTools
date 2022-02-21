2898222826f6d84b0836c6429d75c606c0ddba32
mostobriv <15954313+mostobriv@users.noreply.github.com>
2022-02-21 14:51:13 +0700

    Update member_double_click.py

--- HexRaysPyTools/callbacks/member_double_click.py
+++ HexRaysPyTools/callbacks/member_double_click.py
@@ -1,4 +1,5 @@
 import idaapi
+import idc
 import ida_hexrays
 
 from . import callbacks
@@ -10,17 +11,20 @@ class MemberDoubleClick(callbacks.HexRaysEventHandler):
         super(MemberDoubleClick, self).__init__()
 
     def handle(self, event, *args):
+        # print("wtf")
         hx_view = args[0]
         item = hx_view.item
         if item.citype == idaapi.VDI_EXPR and item.e.op in (idaapi.cot_memptr, idaapi.cot_memref):
             # Look if we double clicked on expression that is member pointer. Then get tinfo_t of  the structure.
             # After that remove pointer and get member name with the same offset
             if item.e.x.op == idaapi.cot_memref and item.e.x.x.op == idaapi.cot_memptr:
+                # print('1')
                 vtable_tinfo = item.e.x.type.get_pointed_object()
                 method_offset = item.e.m
                 class_tinfo = item.e.x.x.x.type.get_pointed_object()
                 vtable_offset = item.e.x.x.m
             elif item.e.x.op == idaapi.cot_memptr:
+                # print('2')
                 vtable_tinfo = item.e.x.type
                 if vtable_tinfo.is_ptr():
                     vtable_tinfo = vtable_tinfo.get_pointed_object()
@@ -28,13 +32,24 @@ class MemberDoubleClick(callbacks.HexRaysEventHandler):
                 class_tinfo = item.e.x.x.type.get_pointed_object()
                 vtable_offset = item.e.x.m
             else:
+                # print("inner branch")
                 func_offset = item.e.m
                 struct_tinfo = item.e.x.type.get_pointed_object()
+                item_ea = item.e.ea
+                if item_ea == idaapi.BADADDR: # quick-fix
+                    item_ea = idc.here()
+                    
                 func_ea = helper.choose_virtual_func_address(helper.get_member_name(struct_tinfo, func_offset))
                 if func_ea:
                     idaapi.jumpto(func_ea)
-
+                
+                if not item.e.x.type.is_ptr():
+                    struct_tinfo = item.e.x.type
+                else:
+                    struct_tinfo = item.e.x.type.get_pointed_object()
+                
                 sid = idaapi.get_struc_id(struct_tinfo.dstr())
+                # print(hex(sid))
                 if sid != idaapi.BADADDR:
                     sptr = idaapi.get_struc(sid)
                     mid = idaapi.get_member_id(sptr, func_offset)
@@ -48,7 +63,7 @@ class MemberDoubleClick(callbacks.HexRaysEventHandler):
                                 tl.ea = target_func.body.ea
                                 tl.itp = ida_hexrays.ITP_SEMI
                                 old_comment = target_func.get_user_cmt(tl, 0)
-                                jmp_src = item.e.ea
+                                jmp_src = item_ea
                                 src_as_string = "0x{:x}".format(jmp_src)
                                 if old_comment is None:
                                     old_comment = "CALLED_FROM =>"
@@ -65,7 +80,8 @@ class MemberDoubleClick(callbacks.HexRaysEventHandler):
 
             func_name = helper.get_member_name(vtable_tinfo, method_offset)
             func_ea = helper.choose_virtual_func_address(func_name, class_tinfo, vtable_offset)
-
+            
+            # print("outer branch")
             if not func_ea:
                 sid = idaapi.get_struc_id(vtable_tinfo.get_type_name())
                 if sid != idaapi.BADADDR:
@@ -78,6 +94,7 @@ class MemberDoubleClick(callbacks.HexRaysEventHandler):
                             func_ea = commented_address
                         except:
                             pass
+
 
             if func_ea:
                 target_func = idaapi.decompile(func_ea)
@@ -94,7 +111,6 @@ class MemberDoubleClick(callbacks.HexRaysEventHandler):
                     target_func.save_user_cmts()
                 idaapi.jumpto(func_ea)
                 return 1
-                
 
 
 callbacks.hx_callback_manager.register(idaapi.hxe_double_click, MemberDoubleClick())
